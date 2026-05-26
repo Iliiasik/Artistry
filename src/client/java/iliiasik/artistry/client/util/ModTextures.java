348765a237584ef854ab1625d20ff478ec4a7b52
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2026-05-26 16:44:19 +0600

    added pixelize and delete buttons

--- src/client/java/iliiasik/artistry/client/util/ModTextures.java
+++ src/client/java/iliiasik/artistry/client/util/ModTextures.java
@@ -9,14 +9,17 @@ public final class ModTextures {
      **/
 
     public static final Identifier FRAME            = Identifier.of("artistry", "textures/ui/frame.png");
-    public static final Identifier PALETTE_BLOCKS = Identifier.of("artistry", "textures/ui/palette_blocks.png");
-    public static final Identifier PALETTE_HEX = Identifier.of("artistry", "textures/ui/palette_hex.png");
+    public static final Identifier PALETTE_BLOCKS   = Identifier.of("artistry", "textures/ui/palette_blocks.png");
+    public static final Identifier PALETTE_HEX      = Identifier.of("artistry", "textures/ui/palette_hex.png");
     public static final Identifier BRUSH            = Identifier.of("artistry", "textures/ui/brush.png");
     public static final Identifier BRUSH_ACTIVE     = Identifier.of("artistry", "textures/ui/brush_active.png");
     public static final Identifier ERASER           = Identifier.of("artistry", "textures/ui/eraser.png");
     public static final Identifier ERASER_ACTIVE    = Identifier.of("artistry", "textures/ui/eraser_active.png");
     public static final Identifier PIPETTE          = Identifier.of("artistry", "textures/ui/pipette.png");
     public static final Identifier PIPETTE_ACTIVE   = Identifier.of("artistry", "textures/ui/pipette_active.png");
+    public static final Identifier IMAGE            = Identifier.of("artistry", "textures/ui/image.png");
+    public static final Identifier PIXELIZE         = Identifier.of("artistry", "textures/ui/pixelize.png");
+    public static final Identifier DELETE           = Identifier.of("artistry", "textures/ui/delete.png");
     public static final Identifier SIZE_SWITCHER    = Identifier.of("artistry", "textures/ui/size_switcher.png");
     public static final Identifier PALETTE_SWITCHER = Identifier.of("artistry", "textures/ui/palette_switcher.png");
     public static final Identifier SIZE_SCREEN      = Identifier.of("artistry", "textures/ui/size_screen.png");
