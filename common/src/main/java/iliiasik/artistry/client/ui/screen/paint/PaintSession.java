1e536182559659b86754f627804780212c9a40be
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2026-09-24 02:05:21 +0600

    Version 1.2.2 bug fix

--- common/src/main/java/iliiasik/artistry/client/ui/screen/paint/PaintSession.java
+++ common/src/main/java/iliiasik/artistry/client/ui/screen/paint/PaintSession.java
@@ -210,6 +210,7 @@ public class PaintSession {
     }
 
     public void lockImage(UUID uuid, boolean lock) {
+        if (lock) mirrorImageLayerToWorld();
         ArtistryNetwork.sendToServer(new LockCanvasImageC2SPacket(target(), uuid, lock));
     }
 
